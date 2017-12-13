46acddd80f4ede4bb46d12014be2395cbc03b1dc
Constantine Bindas <constantine.bindas@gmail.com>
2017-12-13 11:25:40 +0100

    Remove unused import

--- src/AppBundle/Controller/BatteryLogController.php
+++ src/AppBundle/Controller/BatteryLogController.php
@@ -4,7 +4,6 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Form\BatteryLogForm;
 use AppBundle\Form\Handler\BasicFormHandler;
 use Symfony\Component\HttpFoundation\Request;
